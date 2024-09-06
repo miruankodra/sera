33131db79600123a911621acf0dbd93f4ca7c2e4
miruan kodra <miruankodra@gmail.com>
2024-09-06 22:08:53 +0200

    Login functionality start

--- src/app/pages/authentication/login/login.component.ts
+++ src/app/pages/authentication/login/login.component.ts
@@ -1,8 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {SeTitleComponent} from "../../../components/shared/se-title/se-title.component";
 import {environment} from "../../../../environments/environment";
 import {SeInputComponent} from "../../../components/shared/se-input/se-input.component";
 import {SeButtonComponent} from "../../../components/shared/se-button/se-button.component";
+import {HttpService} from "../../../services/http.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'se-login',
@@ -18,5 +20,18 @@ import {SeButtonComponent} from "../../../components/shared/se-button/se-button.
 export class LoginComponent {
 
   appName = environment.APP_NAME
+  private _httpService: HttpService = inject(HttpService);
+  private formBuilder = inject(FormBuilder);
+  loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.min(8)]],
+    });
+  }
+
+
+  // async login(): Promise
 
 }
